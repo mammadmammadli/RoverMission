f6f65da53bbf2323c90ef726148388e8d8a77867
Mammad <memmed199@gmail.com>
2021-06-09 17:20:52 +0400

    Describe tests for Rover.

---
+++ __tests__/rover.spec.js
@@ -0,0 +1,7 @@
+
+
+describe("Rover", () => {
+    it.todo("Moves in each direction");
+    it.todo("Reports current coordinates correctly");
+    it.todo("Reports current coordinates to world correctly");
+});

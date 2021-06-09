4e1d1e013fcd918c101f7c531b162c07469744b2
Mammad <memmed199@gmail.com>
2021-06-09 17:32:46 +0400

    Implement test cases for Rover.

--- __tests__/rover.spec.js
+++ __tests__/rover.spec.js
@@ -1,7 +1,31 @@
-
+const request = require("supertest");
+const Rover = require("../rover.js");
+const app = require("../app.js");
 
 describe("Rover", () => {
-    it.todo("Moves in each direction");
-    it.todo("Reports current coordinates correctly");
-    it.todo("Reports current coordinates to world correctly");
+    it("Moves in each direction", function () {
+        const rover = new Rover();
+        rover.move("F");
+        expect(rover.coordinate.y).toBe(1);
+        rover.move("B");
+        expect(rover.coordinate.y).toBe(0);
+        rover.move("L");
+        expect(rover.coordinate.x).toBe(-1);
+        rover.move("R");
+        expect(rover.coordinate.x).toBe(0);
+    });
+
+    it("Reports current coordinates correctly", () => {
+        const rover = new Rover();
+        rover.move("FFL");
+        expect(rover.getCoordinates()).toBe("(-1, 2) WEST");
+    });
+
+    it("Moves in Mars", () => {
+        request(app).post('/move', "FLFFFRFLB").then(response => {
+            expect(response.body).toBe("(-1, 4) SOUTH");
+            expect(response.statusCode).toBe(200);
+            done();
+        })
+    });
 });

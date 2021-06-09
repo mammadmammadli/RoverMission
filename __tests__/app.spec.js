3da1f14729f409f01388ac5d7dc07f2234ccba79
Mammad <memmed199@gmail.com>
2021-06-09 16:46:55 +0400

    Implement tests for Application

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -2,5 +2,11 @@ const request = require("supertest");
 const app = require("../app.js");
 
 describe("Application", () => {
-    test.todo("Health check");
+    test.todo("Health check", done => {
+        request(app).get('/').then(response => {
+            expect(response.body === "ok");
+            expect(response.statusCode === 200 || 201);
+            done();
+        })
+    });
 });
